40145cad902c83543ffa822f7691e2db3503cf5b
t_tr <tristan.treboz@gmx.fr>
2020-03-20 23:24:09 +0100

    Inheritance

--- Car/Program.cs
+++ Car/Program.cs
@@ -2,15 +2,34 @@
 
 namespace Car
 {
-        class Car
+    class Vehicle  // base class (parent) 
+    {
+        public string brand = "Ford";  // Vehicle field
+        public void honk()             // Vehicle method 
         {
-            string color = "red";
-
-            static void Main(string[] args)
-            {
-                Car myObj = new Car();
-                Console.WriteLine(myObj.color);
-            }
+            Console.WriteLine("Tuut, tuut!");
         }
     }
+
+    class Car : Vehicle  // derived class (child)
+    {
+        public string modelName = "Mustang";  // Car field
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a myCar object
+            Car myCar = new Car();
+
+            // Call the honk() method (From the Vehicle class) on the myCar object
+            myCar.honk();
+
+            // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+        }
+    }
+
+}
 
