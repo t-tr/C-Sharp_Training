c2c15a2365c97f9ee0b199855a3283fa09ee0280
t_tr <tristan.treboz@gmx.fr>
2020-03-21 16:46:51 +0100

    Add File project

---
+++ File/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace File
+{
+    using System.IO;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string writeText = "Hello World!";  // Create a text string
+            File.WriteAllText("filename.txt", writeText);  // Create a file and write the content of writeText to it
+
+            string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
+            Console.WriteLine(readText);  // Output the content        
+
+            File.Delete("filename.txt");
+
+        }
+    }
+}
