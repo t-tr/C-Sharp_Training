cd74f466d5896c447eecaf82b326c28742040080
t_tr <tristan.treboz@gmx.fr>
2020-03-29 23:50:37 +0200

    Call countingValleys in Program

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -38,7 +38,7 @@ namespace HackerRank
             */
 
             Console.WriteLine("SockMerchant:" + BasicChallenges.sockMerchant(9, new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20 }));
-
+            Console.WriteLine("countingValleys:" + BasicChallenges.countingValleys(12, "DDUUDDUDUUUD"));
 
         }
 
