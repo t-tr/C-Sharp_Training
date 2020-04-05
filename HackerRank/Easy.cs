ed472186e64bcfc7aaaad008f19fd39af5f311cf
t-tr <tristan.treboz@gmx.fr>
2020-04-05 23:08:24 +0200

    Add comment to plusMinus

--- HackerRank/Easy.cs
+++ HackerRank/Easy.cs
@@ -242,6 +242,10 @@ namespace HackerRank
 
         }
 
+        /*
+         * Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros.
+         * Print the decimal value of each fraction on a new line.
+         */
         public static void plusMinus(int[] arr)
         {
 
